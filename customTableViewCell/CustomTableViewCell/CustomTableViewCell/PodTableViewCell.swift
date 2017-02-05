8422305cb0330b415211fdbcbc6e80c9481bcb62
sashohadz <sasho.hadzhiev@yahoo.com>
2017-02-05 19:13:43 +0200

    ViewControllers implementation update.

--- customTableViewCell/CustomTableViewCell/CustomTableViewCell/PodTableViewCell.swift
+++ customTableViewCell/CustomTableViewCell/CustomTableViewCell/PodTableViewCell.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol PodsTableViewCellDelegate{
+    func didPressBookmarkButton(inCell:PodTableViewCell)
+}
+
 class PodTableViewCell: UITableViewCell {
 
     @IBOutlet weak var podImageView: UIImageView!
@@ -15,19 +19,46 @@ class PodTableViewCell: UITableViewCell {
     @IBOutlet weak var podDetailLabel: UILabel!
     @IBOutlet weak var bookmarkButton: UIButton!
     
+    var delegate: PodsTableViewCellDelegate?
+    
+    var isBookmarked:Bool! = false{
+        didSet{
+            switch isBookmarked{
+            case true:
+                self.podImageView.layer.borderWidth = 4.0
+                self.podImageView.layer.borderColor = UIColor.yellow.cgColor
+            case false:
+                self.podImageView.layer.borderWidth = 0.0
+
+            default:
+                break
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.podImageView.layer.cornerRadius = self.podImageView.frame.width / 2
+        self.bookmarkButton.isSelected = false
         
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
     @IBAction func bookmarkSelected(_ sender: UIButton) {
+        self.buttonWasPressed()
+        if (!isBookmarked) {
+            isBookmarked = true
+            self.bookmarkButton.isSelected = true
+        } else {
+            isBookmarked = false
+            self.bookmarkButton.isSelected = false
+        }
+    }
+    
+    func buttonWasPressed() {
+        self.delegate?.didPressBookmarkButton(inCell: self)
     }
 }
--- customTableViewCell/CustomTableViewCell/CustomTableViewCell/PodTableViewController.swift
+++ customTableViewCell/CustomTableViewCell/CustomTableViewCell/PodTableViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class PodTableViewController: UITableViewController {
 
+enum podsDataType{
+    case podImage, podName, podLink, podDescription, isBookmarked
+}
+
+class PodTableViewController: UITableViewController, PodsTableViewCellDelegate {
+
+    let podsData:[[podsDataType:String]] = [
+        [.podImage:"leanplum.png",
+         .podName:"Leanplum",
+         .podLink:"https://cocoapods.org/pods/Leanplum-iOS-SDK",
+         .podDescription:"Messaging, Automation, App Editing, Personalization, A/B Testing, and Analytics",
+         .isBookmarked:"false"],
+        [.podImage:"dynamicButton.png",
+         .podName:"DynamicButton",
+         .podLink:"https://cocoapods.org/pods/DynamicButton",
+         .podDescription:"Flat design button written in Swift",
+         .isBookmarked:"false"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,66 +43,37 @@ class PodTableViewController: UITableViewController {
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 10
+        return self.podsData.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PodTableViewCellIdentifier", for: indexPath)
         
+        let data = self.podsData[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PodTableViewCellIdentifier", for: indexPath) as! PodTableViewCell
+        cell.podNameLabel.text = data[.podName]
+        cell.podDetailLabel.text = data[.podDescription]
+        cell.podImageView.image = UIImage(named: data[.podImage]!)
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func didPressBookmarkButton(inCell:PodTableViewCell) {
+        inCell.isBookmarked = !inCell.isBookmarked
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.destination is PodDetailViewController{
+            let destination = segue.destination as! PodDetailViewController
+            destination.loadViewIfNeeded()
+            destination.podNameLabel.text = self.podsData[self.tableView.indexPathForSelectedRow!.row][.podName]
+            destination.podDetailsLabel.text = self.podsData[self.tableView.indexPathForSelectedRow!.row][.podDescription]
+        }
     }
-    */
-
 }
